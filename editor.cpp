d2b248a3a529d4aeb82d161ea0f3ccb67c0aec1f
remeh <remeh@remeh.fr>
2020-04-19 14:51:58 +0200

    editor: support ct and cT

--- editor.cpp
+++ editor.cpp
@@ -332,7 +332,7 @@ void Editor::keyPressEventNormal(QKeyEvent* event, bool ctrl, bool shift) {
 	}
 }
 
-void Editor::keyPressEventSubMode(QKeyEvent* event, bool ctrl, bool shift) {
+void Editor::keyPressEventSubMode(QKeyEvent* event, bool, bool shift) {
 	if (event->key() ==  Qt::Key_Escape) {
 		this->setSubMode(NO_SUBMODE);
 		this->setMode(MODE_NORMAL);
@@ -347,24 +347,76 @@ void Editor::keyPressEventSubMode(QKeyEvent* event, bool ctrl, bool shift) {
 
 	switch (this->subMode) {
 		case SUBMODE_c:
-			if (!shift && event->key() == Qt::Key_C) {
-				QTextCursor cursor = this->textCursor();
-				QString indent = this->currentLineIndent();
-				cursor.beginEditBlock();
-				this->deleteCurrentLine();
-				this->insertPlainText(indent);
-				cursor.endEditBlock();
-				this->setMode(MODE_INSERT);
-				this->setSubMode(NO_SUBMODE);
-			return;
+			switch (event->key()) {
+				case Qt::Key_C:
+					if (!shift) {
+						QTextCursor cursor = this->textCursor();
+						QString indent = this->currentLineIndent();
+						cursor.beginEditBlock();
+						this->deleteCurrentLine();
+						this->insertPlainText(indent);
+						cursor.endEditBlock();
+						this->setMode(MODE_INSERT);
+						this->setSubMode(NO_SUBMODE);
+						return;
+					}
+					break;
+				case Qt::Key_F:
+					if (shift) {
+						this->setSubMode(SUBMODE_cF);
+						return;
+					} else {
+						this->setSubMode(SUBMODE_cf);
+						return;
+					}
+				case Qt::Key_T:
+					if (shift) {
+						this->setSubMode(SUBMODE_cT);
+						return;
+					} else {
+						this->setSubMode(SUBMODE_ct);
+						return;
+					}
 			}
 			break;
 		case SUBMODE_cf:
-			// TODO(remy): implement
-			break;
 		case SUBMODE_cF:
-			// TODO(remy): implement
-			break;
+		case SUBMODE_ct:
+		case SUBMODE_cT:
+			{
+				int distance = 0;
+				if (this->subMode == SUBMODE_cf || this->subMode == SUBMODE_ct) {
+					distance = this->findNextOneInCurrentLine(event->text()[0]);
+				} else {
+					distance = this->findPreviousOneInCurrentLine(event->text()[0]);
+				}
+
+				if (distance == 0) {
+					// nothing has been found
+					break;
+				}
+
+				if (this->subMode == SUBMODE_cf) {
+					distance += 1;
+				} else if (this->subMode == SUBMODE_cT) {
+					distance -= 1;
+				}
+
+				QTextCursor cursor = this->textCursor();
+				cursor.beginEditBlock();
+				if (this->subMode == SUBMODE_cf || this->subMode == SUBMODE_ct) {
+					cursor.movePosition(QTextCursor::Right, QTextCursor::KeepAnchor, distance);
+				} else {
+					cursor.movePosition(QTextCursor::Left, QTextCursor::KeepAnchor, distance);
+				}
+				this->setTextCursor(cursor);
+				this->cut();
+				cursor.endEditBlock();
+			}
+			this->setMode(MODE_INSERT);
+			this->setSubMode(NO_SUBMODE);
+			return;
+
 		case SUBMODE_f:
 			{
 				int distance = this->findNextOneInCurrentLine(event->text()[0]);
--- mode.h
+++ mode.h
@@ -9,6 +9,8 @@
 #define SUBMODE_c 1
 #define SUBMODE_cf 2
 #define SUBMODE_cF 3
-#define SUBMODE_f 4
-#define SUBMODE_F 5
-#define SUBMODE_d 6
+#define SUBMODE_ct 4
+#define SUBMODE_cT 5
+#define SUBMODE_f 6
+#define SUBMODE_F 7
+#define SUBMODE_d 8
