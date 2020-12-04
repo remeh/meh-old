43e4f4e05940d88acd3df98da4e611ec5806531f
remeh <remeh@remeh.fr>
2020-12-04 17:30:36 +0100

    main: read on stdin with -

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <QApplication>
+#include <QFile>
 #include <QObject>
 #include <QStringList>
+#include <QThread>
 
+#include <stdio.h>
+
+#include "buffer.h"
 #include "editor.h"
 #include "window.h"
+
+#include "qdebug.h"
 
 int main(int argv, char **args)
 {
@@ -14,7 +21,6 @@ int main(int argv, char **args)
     QCoreApplication::setOrganizationDomain("remy.io");
     QCoreApplication::setApplicationName("meh");
 
-
     Window window;
     window.setWindowTitle(QObject::tr("meh - no file"));
     window.resize(800, 700);
@@ -22,7 +28,24 @@ int main(int argv, char **args)
     window.show();
 
     QStringList arguments = QCoreApplication::arguments();
-    if (arguments.size() > 0) {
+
+    // special case of reading from stdin
+    if (arguments.size() > 1 && arguments.at(1) == "-") {
+
+        QByteArray content;
+
+        QFile in;
+        if (!in.open(stdin, QIODevice::ReadOnly)) {
+            qWarning() << "can't read stdin";
+            qWarning() << in.errorString();
+        }
+        content += in.readAll();
+        in.close();
+
+        Buffer* buffer = new Buffer(content);
+        buffer->setName("stdin");
+        window.getEditor()->setCurrentBuffer(buffer);
+    } else if (arguments.size() > 0) {
         for (int i = arguments.size() - 1; i > 0; i--) {
             if (arguments.at(i).startsWith("+")) {
                 continue;
